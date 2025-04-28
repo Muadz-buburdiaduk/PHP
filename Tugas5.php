c2c0776418adcf07684d1cbb79274cf6e4201520
Muadz-buburdiaduk <muadzkurumi@gmail.com>
2025-04-28 08:44:42 +0800

    Add files via upload

---
+++ Tugas5.php
@@ -0,0 +1,9 @@
+<?php
+$nilai_matematika = 65;
+$nilai_bahasa = 55;
+
+if ($nilai_matematika >=80 && $nilai_bahasa >=70) {
+    echo "Selamat! kamu boleh nonton bioskop di akhir pekan ini";
+} else {
+    echo "Yahh... belajar lagi deh mending";
+}
